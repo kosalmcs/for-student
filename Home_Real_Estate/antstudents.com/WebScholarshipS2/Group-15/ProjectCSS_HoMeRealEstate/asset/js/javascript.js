845ce913875b95456441c77f9923293c9a5c1ac2
kosalmcs <94227040+kosalmcs@users.noreply.github.com>
2025-03-04 22:18:14 +0700

    add Home_Real_Estate 04.03.2025

---
+++ Home_Real_Estate/antstudents.com/WebScholarshipS2/Group-15/ProjectCSS_HoMeRealEstate/asset/js/javascript.js
@@ -0,0 +1,227 @@
+$(document).ready(function() {
+  $('[data-bs-toggle="collapse"]').on('click', function(event) {
+      event.stopPropagation();
+      var target = $(this).attr('href');
+      $(target).collapse('toggle'); // Toggle the targeted collapse
+  });
+
+  $(document).on('click', function() {
+      $('.collapse').collapse('hide');
+  });
+
+  $('.collapse').on('click', function(event) {
+      event.stopPropagation();
+  });
+
+  function checkScreenWidth() {
+      if ($(window).width() >= 992) {
+          $('.collapse').collapse('hide');
+      }
+  }
+
+  checkScreenWidth();
+  $(window).on('resize', checkScreenWidth);
+});
+
+
+// carousel swiper scroll ----- 
+$(document).ready(function () {
+    var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+  );
+  if (window.matchMedia("(min-width: 576px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+      wrap: false
+    });
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 3) {
+        scrollPosition += cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 1) {
+        scrollPosition -= cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+});
+
+// ------------------- validation email 
+ // JavaScript to handle form validation
+ (function () {
+  'use strict';
+
+  // Fetch the form and the submit button
+  var form = document.getElementById('forgetPwForm');
+  var submitButton = document.getElementById('submitButton');
+
+  form.addEventListener('submit', function (event) {
+    // Check form validity
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+      // Remove modal attributes if form is invalid
+      submitButton.removeAttribute('data-bs-toggle');
+      submitButton.removeAttribute('data-bs-target');
+    } else {
+      // Set modal attributes if form is valid
+      submitButton.setAttribute('data-bs-toggle', 'modal');
+      submitButton.setAttribute('data-bs-target', '#create-pw');
+    }
+
+    form.classList.add('was-validated');
+  }, false);
+})();
+
+
+// ---------- tooltips --
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// -------- registerr modal ---------- 
+
+
+// ----------- modal 
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
+
+
+
+
+
+
+
+// ------------ toast script ------
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+  // ----------- heart-click ------
+  $(document).ready(function () {
+    $('.heart-click').on('click', function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        $(this).toggleClass('active');
+    });
+  });
+
+
+
+// ------------- form validation ---------
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+
+
+
+
+// ===========Typing Text==============
+
+        var TxtType = function (el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+        };
+
+        TxtType.prototype.tick = function () {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
+
+            if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+
+            this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+            var that = this;
+            var delta = 200 - Math.random() * 100;
+
+            if (this.isDeleting) { delta /= 2; }
+
+            if (!this.isDeleting && this.txt === fullTxt) {
+                delta = this.period;
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                this.loopNum++;
+                delta = 500;
+            }
+
+            setTimeout(function () {
+                that.tick();
+            }, delta);
+        };
+
+        window.onload = function () {
+            var elements = document.getElementsByClassName('typewrite');
+            for (var i = 0; i < elements.length; i++) {
+                var toRotate = elements[i].getAttribute('data-type');
+                var period = elements[i].getAttribute('data-period');
+                if (toRotate) {
+                    new TxtType(elements[i], JSON.parse(toRotate), period);
+                }
+            }
+            // INJECT CSS
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+            document.body.appendChild(css);
+        };
